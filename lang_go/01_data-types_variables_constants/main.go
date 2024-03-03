1bc226fc98cc812c8b3c50610dd1c5fc5f01e9a7
pinejuice <wata_82_10@yahoo.co.jp>
2024-03-03 16:22:55 +0900

    配列とスライスについて追記

--- lang_go/01_data-types_variables_constants/main.go
+++ lang_go/01_data-types_variables_constants/main.go
@@ -80,11 +80,54 @@ func main() {
 	str3[0] = 'h'
 	str2 = string(str3)
 	fmt.Println(str2)
-
 	// 複数行の文字列の宣言
 	var str4 = `今日の
 夕飯は
 お寿司でした。
 `
 	fmt.Println(str4)
+
+	// 配列
+	var list1 [5]int
+	list1[0] = 10
+	list1[1] = 20
+	fmt.Println(list1)
+	fmt.Println(list1[3])
+
+	// スライス
+	var slice1 []int
+	fmt.Println(slice1)
+	var slice2 = make([]int, 3)
+	fmt.Println("slice2: ", slice2)
+	fmt.Println("slice2[2]: ", slice2[2])
+	slice2 = append(slice2, 10)
+	fmt.Println("slice2: ", slice2)
+	var slice3 = []int{1, 2, 3}
+	fmt.Println("slice3: ", slice3)
+	fmt.Println("slice3[1]: ", slice3[1])
+	// スライスの要素の削除
+	// (1)の場合
+	var slice4 = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println("slice4: ", slice4)
+	var slice5 = make([]int, 0)
+	for i := 1; i < 11; i++ {
+		if i%2 == 0 {
+			slice5 = append(slice5, i)
+		}
+	}
+	fmt.Println("slice5: ", slice5)
+	slice4 = slice5
+	fmt.Println("slice4: ", slice4)
+	// (2)の場合
+	var slice6 = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println("slice6: ", slice6) // [1 2 3 4 5 6 7 8 9 10]
+	target_index1 := 4
+	slice6 = append(slice6[:target_index1], slice6[target_index1+1:]...)
+	fmt.Println("slice6: ", slice6)
+	// (3)の場合
+	var slice7 = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println("slice7: ", slice7) // [1 2 3 4 5 6 7 8 9 10]
+	n := 4
+	slice7 = slice7[:n+copy(slice7[n:], slice7[n+1:])]
+	fmt.Println("slice7: ", slice7) // [1 2 3 4 6 7 8 9 10]
 }
